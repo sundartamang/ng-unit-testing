a406f5b0968d142d91cdb2ed63e8cfca1d20c71c
sursasundar <sundar.tamang@sursatech.com>
2024-07-13 15:54:24 +0545

    feat: DOM interactions

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,20 +1,48 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { HomeComponent } from './home.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [HomeComponent]
-    }).compileComponents().then(() => {
-      fixture = TestBed.createComponent(HomeComponent);
-      component = fixture.componentInstance;
-    });
+    })
+      .compileComponents();
   }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    el = fixture.debugElement;
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('it should render a button a text Subscribe', () => {
+    const btnElement = el.queryAll(By.css('.subscribe'));
+    component.btnText = "Subscribe";
+    component.isSubscribe = false;
+    fixture.detectChanges();
+    expect(btnElement[0].nativeElement.textContent).toBe('Subscribe');
+    expect(btnElement[0].nativeElement.disabled).toBeFalse();
+  });
+
+  it('button a text Subscribed and button disable when clicked', () => {
+    const btnElement = el.queryAll(By.css('.subscribe'));
+    component.btnText = "Subscribe";
+    component.isSubscribe = false;
+    btnElement[0].nativeElement.click(); // click event before change detection
+    fixture.detectChanges();
+    expect(btnElement[0].nativeElement.textContent).toBe('Subscribed');
+    expect(btnElement[0].nativeElement.disabled).toBeTrue();
+  })
 });
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,11 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  btnText: string = "Subscribe";
+  isSubscribe: boolean = false;
 
-  ngOnInit(): void {
+
+  subscribe(){
+    this.btnText = "Subscribed";
+    this.isSubscribe = true;
   }
 
 }
